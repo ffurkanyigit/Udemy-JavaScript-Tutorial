2ea90d9e145de98af06a24ba93316021cb701941
ffurkanyigit <f.furkanyigit@gmail.com>
2023-06-17 00:46:46 +0300

    revize

--- Temeller/Switch Case/app.js
+++ Temeller/Switch Case/app.js
@@ -1,8 +1,8 @@
-const process = 1;
+const process = 1; // değişkene 1 dediğimiz için case 1' e girer. Değişken 2 veya 3 olsaydı, case 2 veya case 3' e girerdik.
 switch (process) {
     case 1:
         console.log("işlem 1"); // işlem 1 gerçekleşir ekrana yazı yazılır ve break komutuna gelir. 
-        break; // İşlem başarılı switch-case yapısından çık demektir. Eğer break kullanmazsak diğer case'lere de break görene kadar girer. 
+        break; // İşlem başarılı switch-case yapısından çık demektir. Eğer break kullanmazsak diğer case'lerde break görene kadar girer. 
     case 2:
         console.log("işlem 2");
         break;
